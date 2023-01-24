3294cccfc677a88bfcc203358c02a547266f9630
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-24 16:01:59 +0200

    add javascript

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,7 +10,7 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
+    this._beat = new Beat();
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
@@ -18,8 +18,25 @@ export default class Application extends EventEmitter {
     message.classList.add("message");
     message.innerText = "Ah";
 
+    this._beat.on(Beat.events.BIT, () => {
+
+//console.log(lyrics.length+"="+count);
+      if (count == lyrics.length-1) {
+        count = 0;
+      } else {
+        count++;
+      }
+      this._create(lyrics[count]);
+    });
+
     document.querySelector(".main").appendChild(message);
 
     this.emit(Application.events.READY);
+  }
+  _create(messageText) {
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = messageText;
+    document.querySelector(".main").appendChild(message);
   }
 }
